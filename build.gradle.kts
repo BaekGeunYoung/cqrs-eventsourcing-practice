43b11bd14ff14aee672ab95428d2a7813e9d0eb4
BaekGeunYoung <dvmflstm@gmail.com>
2020-06-20 18:01:26 +0900

    swagger testing을 위해 의존성 모듈 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,8 @@ dependencies {
     implementation("org.axonframework:axon-spring-boot-starter:4.0.3") {
         exclude(group = "org.axonframework", module = "axon-server-connector")
     }
+    implementation("io.springfox:springfox-swagger2:2.9.2")
+    implementation("io.springfox:springfox-swagger-ui:2.9.2")
     runtimeOnly("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
